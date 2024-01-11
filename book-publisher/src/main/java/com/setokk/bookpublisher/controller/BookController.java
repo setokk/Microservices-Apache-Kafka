e95c0d3c99bcaed9aec73730a2797193c4586b38
setokk <tesmtracks@gmail.com>
2024-01-11 15:26:52 +0200

    Fixed db port issue

--- book-publisher/src/main/java/com/setokk/bookpublisher/controller/BookController.java
+++ book-publisher/src/main/java/com/setokk/bookpublisher/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.setokk.bookpublisher.controller;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.setokk.bookpublisher.request.BookRequest;
